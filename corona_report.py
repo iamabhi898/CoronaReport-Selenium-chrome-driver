76f397970872eaf8a0b05a5d0b9eadf5dfdbb6fc
Abhishek Tripathi <59258140+iamabhi898@users.noreply.github.com>
2020-05-12 19:14:22 +0530

    Update corona_report.py

--- corona_report.py
+++ corona_report.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 PATH = "/home/abhishek/Documents/chromedriver.exe"
 entry = input("Worldwide(W) or India(I) ? : \n")
