97be5a9443d68cff6811db99aed2c6374c82f91a
Abhishek Tripathi <59258140+iamabhi898@users.noreply.github.com>
2020-05-12 19:13:31 +0530

    Add files via upload

---
+++ corona_report.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+PATH = "/home/abhishek/Documents/chromedriver.exe"
+entry = input("Worldwide(W) or India(I) ? : \n")
+driver = webdriver.Chrome(PATH)
+
+if entry.lower() == "w":
+
+	print("\n\n\n\n")
+	driver.get("https://news.google.com/covid19/map?hl=en-IN&gl=IN&ceid=IN:en")
+	driver.implicitly_wait(10)
+
+	country_list = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[5]/div/div/div[1]/div/div[1]')
+	print(country_list.text)
+	
+	driver.close()
+
+elif entry.lower() == "i":
+
+	print("\n\n\n\n")
+	driver.get('https://www.mohfw.gov.in/')
+	driver.implicitly_wait(5)
+
+	data_button = driver.find_element_by_xpath('//*[@id="site-dashboard"]/div/div/div/div/ul/li[5]/a[2]/img')
+	data_button.click()
+
+	state_list = driver.find_element_by_xpath('//*[@id="state-data"]/div/div/div/div/table')
+	print(state_list.text)
+
+	driver.close()
